97bace6aefe2fa14cfa193e98bbf0937867dd465
Torla <munford.ns@gmail.com>
2017-04-15 15:41:16 +0200

    screen and tileset class

--- game/main.cpp
+++ game/main.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 #include <string>
 //#include "position.h"
-#include "grafic.h"
+#include "screen.h"
 
 
 using namespace std;
 
 
 int main() {
-    window *win = new window("Test",300,300);
-    cout << win->getName() <<  " " << win->getWidth() << " " << win->getHight() << endl;
-    texture *tex = new texture("img/test.jpg",win);
-    win->clear();
-    win->put(tex,10,10,100,100);
-    win->show();
+int i;
+for(i=0;i<100;i++){
+    screen.clear();
+    screen.put(TEST,(i*25)%1000,(i*25)%1000,25,25);
+    screen.show();
+    SDL_Delay(33);
+}
     SDL_Delay(1000);
-    delete win;
     return 0;
 }
 
